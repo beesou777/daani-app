4f3e2150adaca884d5cd9542e03603637b805a22
beesou777 <sbeesou@gmail.com>
2023-06-04 16:25:03 +0545

    commit

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,10 @@ import vue from '@vitejs/plugin-vue'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [vue()],
+  build: {
+    rollupOptions: {
+      external: ["img/undraw_profile_1.svg"],
+    },
+  },
   
 })
