78f64a3b6867524004daed7fac815b178a31c60f
beesou777 <103716649+beesou777@users.noreply.github.com>
2023-07-30 14:43:00 +0545

    Update axios.js

--- src/axios.js
+++ src/axios.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 // const accessToken = JSON.parse(localStorage.getItem('access_token')) || null;
-axios.defaults.baseURL = 'https://eccomerce-production-ec27.up.railway.app/api/';
+axios.defaults.baseURL = 'https://eccomerce-ny3a.onrender.com/api/';
 // axios.defaults.baseURL = 'http://localhost:4000/api/';
 // 
 export default axios;
